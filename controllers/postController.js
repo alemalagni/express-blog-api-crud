14e6e7f96a29e1f537e12a70ddaff2d41393d1ae
alemalagni <alemalagni@gmail.com>
2025-03-31 10:27:14 +0200

    feat: add controller function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,15 +1,25 @@
 const postController = require("../data/menu");
 
-function index(req, res){}
+function index(req, res){
+
+}
 
 function show(req, res){}
 
-function store(req, res){}
+function store(req, res){
+    res.send('Creazione del post');
+}
 
-function modify(req, res){}
+function modify(req, res){
+    res.send('Modifica integrale del post ' + req.params.id);
+}
 
-function update(req, res){}
+function update(req, res){
+    res.send('Modifica parziale del post ' + req.params.id);
+}
 
-function destroy(req, res){}
+function destroy(req, res){
+    res.send('Eliminazione del post ' + req.params.id);
+}
 
 module.exports = { index, show, store, modify, update, destroy }
