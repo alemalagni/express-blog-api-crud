b1183e24743702f7832b8af0f19a1505e11766a9
alemalagni <alemalagni@gmail.com>
2025-03-31 10:21:15 +0200

    feat: add postController script

---
+++ controllers/postController.js
@@ -0,0 +1,15 @@
+const postController = require("../data/menu");
+
+function index(req, res){}
+
+function show(req, res){}
+
+function store(req, res){}
+
+function modify(req, res){}
+
+function update(req, res){}
+
+function destroy(req, res){}
+
+module.exports = { index, show, store, modify, update, destroy }
