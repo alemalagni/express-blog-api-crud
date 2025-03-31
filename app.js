9646d25bc336e0123e7aab8a34a4050708676d11
alemalagni <alemalagni@gmail.com>
2025-03-31 23:04:29 +0200

    fix:

--- app.js
+++ app.js
@@ -2,34 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const src = require('debug');
-const router = express.Router();
 const postsRouter = require("./routers/posts");
-const postController = require('./controllers/postController');
 
 app.use( express.static('public') );
 
 app.use ( "/posts", postsRouter );
 
-// Index
-router.get( '/', postController.index);
-
-// Show
-router.get( '/:id', postController.show);
-
-// Store
-router.post( '/', postController.store);
-
-// Update
-router.put( '/:id', postController.update);
-
-// Modify
-router.patch( '/:id',postController.modify);
-
-// Destroy
-router.delete( '/:id', postController.destroy);
-
 app.listen( port, () => {
     console.log( `App in ascolto sulla porta ${port}` );
 })
-
-module.exports = router;
---
+++ routers/router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('./controllers/postController');
+
+router.get( '/', postController.index); // Index
+router.get( '/:id', postController.show); // Show
+router.post( '/', postController.store); // Store
+router.put( '/:id', postController.update); // Update
+router.patch( '/:id',postController.modify); // Modify
+router.delete( '/:id', postController.destroy); // Destroy
+
+module.exports = router;
