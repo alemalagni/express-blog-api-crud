21cc9247896c8a0712c8719826d1dfae829e2bb4
alemalagni <alemalagni@gmail.com>
2025-03-31 23:06:41 +0200

    fix: app.js

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const src = require('debug');
-const postsRouter = require("./routers/posts");
+//const postsRouter = require("./routers/posts");
 
 app.use( express.static('public') );
-
 app.use ( "/posts", postsRouter );
+
+app.get('/', postController.index); // Index
+app.get('/:id', postController.show); // Show
+app.post('/', postController.store); // Store
+app.put('/:id', postController.update); // Update
+app.patch('/:id', postController.modify); // Modify
+app.delete('/:id', postController.destroy); // Destroy
 
 app.listen( port, () => {
     console.log( `App in ascolto sulla porta ${port}` );
