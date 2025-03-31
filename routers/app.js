6e316aa88735ce4f9a1b33648261274053496ec4
alemalagni <alemalagni@gmail.com>
2025-03-31 10:30:04 +0200

    feat: add new function

--- routers/app.js
+++ routers/app.js
@@ -11,52 +11,10 @@ app.use( express.static('public') );
 app.use ( "/posts", postsRouter );
 
 // Index
-router.get( '/', (req, res) => {
-    res.type('html')
-        .send(index(req));
-});
-
-function index(req){
-    let strIndex = req
-    for ( let i = 0; i < posts.length; i++ ) {
-        strIndex += `
-                    <div>
-                        <h2>${posts[i].title}</h2>
-                        <p>${posts[i].slug}</p>
-                        <div><span>${tag(i)}</span></div>
-                        <div><img src="imgs/posts/${posts[i].image}"></div>
-                        <p>${posts[i].content}</p>
-                    </div>
-                    `
-    }
-
-    return strIndex
-}
-
-function tag(id){
-    let strTags = "";
-    for ( let i = 0; i < posts.length; i++ ) {
-        strTags += ` ${posts[id].tags[i]}`
-    }
-
-    return strTags;
-}
+router.get( '/', postsRouter.index);
 
 // Show
-router.get( '/:id', (req, res) => {
-    const id = req.params.id;
-
-    res.type('html')
-        .send(show(id));
-});
-
-function show(id) {
-    `<h2>${posts[id].title}</h2>
-    <p>${posts[id].slug}</p>
-    <div><span>${tag(id)}</span></div>
-    <div><img src="imgs/posts/${posts[id].image}"></div>
-    <p>${posts[id].content}</p>`
-}
+router.get( '/:id', postsRouter.show);
 
 // Store
 router.post( '/', postsRouter.store);
