85aabd0c2157f2fe54e59eee2a257cf0f76d02d6
alemalagni <alemalagni@gmail.com>
2025-03-31 10:28:52 +0200

    feat: add new controller function

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,50 @@
 const postController = require("../data/menu");
 
 function index(req, res){
-
+    res.type('html')
+        .send(index(req));
 }
 
-function show(req, res){}
+function index(req){
+    let strIndex = req
+    for ( let i = 0; i < posts.length; i++ ) {
+        strIndex += `
+                    <div>
+                        <h2>${posts[i].title}</h2>
+                        <p>${posts[i].slug}</p>
+                        <div><span>${tag(i)}</span></div>
+                        <div><img src="imgs/posts/${posts[i].image}"></div>
+                        <p>${posts[i].content}</p>
+                    </div>
+                    `
+    }
+
+    return strIndex
+}
+
+function tag(id){
+    let strTags = "";
+    for ( let i = 0; i < posts.length; i++ ) {
+        strTags += ` ${posts[id].tags[i]}`
+    }
+
+    return strTags;
+}
+
+function show(req, res){
+    const id = req.params.id;
+
+    res.type('html')
+        .send(show(id));
+}
+
+function show(id) {
+    `<h2>${posts[id].title}</h2>
+    <p>${posts[id].slug}</p>
+    <div><span>${tag(id)}</span></div>
+    <div><img src="imgs/posts/${posts[id].image}"></div>
+    <p>${posts[id].content}</p>`
+}
 
 function store(req, res){
     res.send('Creazione del post');
