ce56faf363a36d48baac34f3d98b398172fe30cd
alemalagni <alemalagni@gmail.com>
2025-04-01 20:25:05 +0200

    feat(app.js): errorHandler function added

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 const postsRouter = require("./routers/router");
 const notFound = require(`./middlewares/notFound.js`);
+const errorHandler = require(`./middlewares/errorHandler.js`);
 
 app.use( express.static('public') );
 app.use( express.json() );
 
 app.use ( "/posts", postsRouter );
 
-app.use(notFound); //Not Found
+app.use(notFound); // Not Found
+app.use(errorHandler); // Messaggio d'errore
 
 app.listen( port, () => {
     console.log( `App in ascolto sulla porta ${port}` );
