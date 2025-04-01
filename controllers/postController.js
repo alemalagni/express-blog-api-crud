91394f5d11f5c484d0717e1f18e45c1feceb3591
alemalagni <alemalagni@gmail.com>
2025-04-01 10:16:27 +0200

    fix: postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -9,7 +9,7 @@ function index(req, res){
                         <h2>${posts[i].title}</h2>
                         <p>${posts[i].slug}</p>
                         <div><span>${tag(i)}</span></div>
-                        <div><img src="imgs/posts/${posts[i].image}"></div>
+                        <div><img src="..${posts[i].image}"></div>
                         <p>${posts[i].content}</p>
                     </div>
                     `
@@ -30,7 +30,7 @@ function show(req, res){
     strIndex = `<h2>${posts[id].title}</h2>
     <p>${posts[id].slug}</p>
     <div><span>${tag(id)}</span></div>
-    <div><img src="${posts[id].image}"></div>
+    <div><img src="..${posts[id].image}"></div>
     <p>${posts[id].content}</p>`;
 
     res.type('html')
