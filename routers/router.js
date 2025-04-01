88a951a09e08917978879927bade21576c16c50d
alemalagni <alemalagni@gmail.com>
2025-04-01 20:15:59 +0200

    feat(router.js): remove NotFound function

--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
-const notFound = require(`./middlewares/notFound.js`);
 
 router.get( '/', postController.index); // Index
 router.get( '/:id', postController.show); // Show
@@ -9,7 +8,5 @@ router.post( '/', postController.store); // Store
 router.put( '/:id', postController.update); // Update
 router.patch( '/:id',postController.modify); // Modify
 router.delete( '/:id', postController.destroy); // Destroy
-
-router.use(notFound); //Not Found
 
 module.exports = router;
