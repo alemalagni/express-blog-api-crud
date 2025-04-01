3d3bb23fcc7e3524497115c4201695dfc91affb3
alemalagni <alemalagni@gmail.com>
2025-04-01 20:10:45 +0200

    feat(app.js): notFound added

--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+const notFound = require(`./middlewares/notFound.js`);
 
 router.get( '/', postController.index); // Index
 router.get( '/:id', postController.show); // Show
@@ -8,5 +9,7 @@ router.post( '/', postController.store); // Store
 router.put( '/:id', postController.update); // Update
 router.patch( '/:id',postController.modify); // Modify
 router.delete( '/:id', postController.destroy); // Destroy
+
+router.use(notFound);
 
 module.exports = router;
