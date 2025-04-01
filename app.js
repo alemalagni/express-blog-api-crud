0f8df95b4d1cb1f8e72f6e288510dd8f746d258a
alemalagni <alemalagni@gmail.com>
2025-04-01 20:02:37 +0200

    fix: notFound declaration

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postsRouter = require("./routers/router");
-const notFound = require(`./middlewares/notFound`);
+const notFound = require(`./middlewares/notFound.js`);
 
 app.use( express.static('public') );
 app.use( express.json() );
