d3c4fd8f0bb8913a0e92399a9ad658449548710f
alemalagni <alemalagni@gmail.com>
2025-04-01 20:29:20 +0200

    fix(notFound.js):

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,8 +1,8 @@
 function notFound (req, res, next) {
     res.json({
         status: 404,
-    error: "Not Found",
-    message: "Pagina non trovata"
+        error: "Not Found",
+        message: "Pagina non trovata"
     });
 };
 
