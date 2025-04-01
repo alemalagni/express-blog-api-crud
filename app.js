55f2eaebcfd15df729a948fba81c89a54f206eff
alemalagni <alemalagni@gmail.com>
2025-04-01 10:08:11 +0200

    fix: app.js

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const src = require('debug');
-//const postsRouter = require("./routers/posts");
+const postsRouter = require("./routers/router");
 const postController = require('./controllers/postController');
 
 app.use( express.static('public') );
-//app.use ( "/posts", postsRouter );
 
-app.get('/', (req, res) => {
-    res.send('Server attivo!');
-});
+app.use ( "/posts", postsRouter );
 
-app.get('/posts', postController.index); // Index
-app.get('/posts/:id', postController.show); // Show
-app.post('/posts', postController.store); // Store
-app.put('/posts/:id', postController.update); // Update
-app.patch('/posts/:id', postController.modify); // Modify
-app.delete('/posts/:id', postController.destroy); // Destroy
+// app.get('/', postController.index); // Index
+// app.get('/:id', postController.show); // Show
+// app.post('/', postController.store); // Store
+// app.put('/:id', postController.update); // Update
+// app.patch('/:id', postController.modify); // Modify
+// app.delete('/:id', postController.destroy); // Destroy
 
 app.listen( port, () => {
     console.log( `App in ascolto sulla porta ${port}` );
