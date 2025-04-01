8cd78b025c5a8d10c7f3ffcc1558150875ecb6c9
alemalagni <alemalagni@gmail.com>
2025-04-01 20:16:16 +0200

    feat(app.js): add notFound function

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postsRouter = require("./routers/router");
+const notFound = require(`./middlewares/notFound.js`);
 
 app.use( express.static('public') );
 app.use( express.json() );
 
 app.use ( "/posts", postsRouter );
+
+router.use(notFound); //Not Found
 
 app.listen( port, () => {
     console.log( `App in ascolto sulla porta ${port}` );
